74a50ffbe3cd00f86cd24ab47179574b7b31f018
Kyle Fong <krfong@ucsc.edu>
2021-01-18 22:04:05 -0800

    Off-by-1 errs reduce/map. Change reduce task assignment and Done() call

--- src/mr/master.go
+++ src/mr/master.go
@@ -11,14 +11,13 @@ import "time"
 
 type Master struct {
   MapTasks map[int]Task
-  ReduceTasks map[int][]string
+  ReduceTasks map[int]bool
+  NMapTasks int
   NReduce int
   HasFinishedJob bool
   Mu sync.Mutex // the master has a Mutex instance because we want one mutex to share amongst workers, and not a different instance each time a function is called
 }
 
-// INSPECT - do we need a task type if it's only for a map task?
-// how can we extend for reduce tasks as well? we need an array
 type Task struct {
   ID int
   File string
@@ -33,25 +32,24 @@ const (
 
 // Your code here -- RPC handlers for the worker to call.
 
-func (m *Master) RequestTask(args *MRTaskArgs, reply *MRTaskReply) error {  
+func (m *Master) RequestTask(args *MRTaskArgs, reply *MRTaskReply) (err error) {  
   
   m.Mu.Lock()
   defer m.Mu.Unlock()
 
-  if (len(m.MapTasks) != 0) {  	
-  	taskID := m.assignMapTask(reply)
-  	m.checkMapTask(taskID)
-  	return nil
+  if (len(m.MapTasks) != 0) {
+    taskID := m.assignMapTask(reply)
+    m.checkMapTask(taskID)
+    return
   }
 
   if (len(m.ReduceTasks) != 0) {
-  	m.assignReduceTask(reply)
-  	return nil
+    m.assignReduceTask(reply)
+    return
   }
 
-  m.HasFinishedJob = true
-
-  return nil  
+  reply.TaskType = ExitTask
+  return
 }
 
 func (m *Master) assignMapTask(reply *MRTaskReply) int {
@@ -82,25 +80,25 @@ func (m *Master) assignMapTask(reply *MRTaskReply) int {
 
 // check if the map task has been completed within a timed duration
 func (m *Master) checkMapTask(mapTaskId int) {
- 	durationOfTime := time.Duration(10) * time.Second
+  durationOfTime := time.Duration(10) * time.Second
   timer := time.AfterFunc(durationOfTime, func() {
-  	m.Mu.Lock()
-  	defer m.Mu.Unlock()
-  	if (m.MapTasks[mapTaskId].State == PROCESSING) {
-  		var mapTask = m.MapTasks[mapTaskId]
-  		mapTask.State = TO_BE_PROCESSED
-  		m.MapTasks[mapTaskId] = mapTask
-  	}
-  	fmt.Println("timer")
+    m.Mu.Lock()
+    defer m.Mu.Unlock()
+    if (m.MapTasks[mapTaskId].State == PROCESSING) {
+      var mapTask = m.MapTasks[mapTaskId]
+      mapTask.State = TO_BE_PROCESSED
+      m.MapTasks[mapTaskId] = mapTask
+    }
+    fmt.Println("timer")
   })
   defer timer.Stop() // time.AfterFunc returns a timer that waits and after a duration calls a function in its own goroutine, we defer stopping the time until the function runs
 }
 
 func (m *Master) assignReduceTask(reply *MRTaskReply) {
-  for i, reduceTask := range m.ReduceTasks {
+  for i := range m.ReduceTasks {
     // assign the map task to the worker
     reply.ReduceTaskID = i
-    reply.Files = reduceTask
+    reply.NMapTasks = m.NMapTasks
     reply.TaskType = ReduceTask
     // remove the reduce task from the list of tasks to be complete
     delete(m.ReduceTasks, i)
@@ -108,19 +106,14 @@ func (m *Master) assignReduceTask(reply *MRTaskReply) {
   }
 }
 
-// TODO: we must guard against updates from workers who have been declared crashed by the master
-// if the wake up and send their call to the master, then they 
 func (m *Master) UpdateMapTask(args *MRTaskUpdate, reply *MRTaskUpdate) error {
-	// must lock this task-state because it is shared and we are performing an update
-	m.Mu.Lock()
-	defer m.Mu.Unlock()
-	// we aren't concerned about ordering, so we can remove the task from the map task slice
-	// this is the equivalent of marking the map task state as PROCESSED
-	delete(m.MapTasks, args.MapTaskID)
-	m.ReduceTasks[args.MapTaskID] = args.Files
-	reply.Success = true
-	
-	return nil
+  m.Mu.Lock()
+  defer m.Mu.Unlock()
+  // this is the equivalent of marking the map task state as PROCESSED
+  delete(m.MapTasks, args.MapTaskID)
+  reply.Success = true
+  
+  return nil
 }
 
 //
@@ -144,11 +137,9 @@ func (m *Master) server() {
 // if the entire job has finished.
 //
 func (m *Master) Done() bool {
- 	// m.Mu.Lock()
- 	// if (len(MapTasks) == 0 && len(ReduceTasks == 0)) {
- 	// 	m.HasFinishedJob = true
- 	// }
- 	// m.Mu.Unlock()
+  if (len(m.MapTasks) == 0 && len(m.ReduceTasks) == 0) {
+    m.HasFinishedJob = true
+  }
   return m.HasFinishedJob
 }
 
@@ -163,8 +154,10 @@ func MakeMaster(tasks []string, nReduce int) *Master {
   
   // initialize map and reduce dictionaries - alternatively, could've used a slice and 2D slice...
   m.MapTasks = make(map[int]Task)
-  m.ReduceTasks = make(map[int][]string)
+  m.ReduceTasks = make(map[int]bool, nReduce)
 
+  // initialize number of tasks
+  m.NMapTasks = len(tasks)
   // initialize number of files to output
   m.NReduce = nReduce
 
@@ -174,27 +167,11 @@ func MakeMaster(tasks []string, nReduce int) *Master {
     m.MapTasks[i] = mapTask
   }
 
+  // create n-many reduce tasks
+  for i := 0; i < nReduce; i++ {
+    m.ReduceTasks[i] = true
+  }
+
   m.server()
   return &m
 }
-
-  // I assume that we should start the clock here when we hand out a task
-  // possible solutions
-  // A:
-  // we initialize a clock for the worker that was issued the map task
-  // when we see the update task rpc call from the worker
-  // we verify the time we started and the time that we see the udapte rpc call
-  // -> problem: this solution relies on the worker to eventually reply - what if the worker crashes and loses data in memory?
-
-  // B:
-  
-
-  // workers will request for tasks
-  // we want to wait for workers to complete the task
-  // if a worker does not complete a task within 10 seconds
-  // then we want to reissue that task to another worker
-
-  // we also have two types of tasks
-  // map tasks and reduce tasks
-  // we want to wait until we have completed all the map tasks
-  // before we hand out map tasks
